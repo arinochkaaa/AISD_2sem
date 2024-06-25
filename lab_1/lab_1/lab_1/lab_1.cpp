be7f7752a5198e3b712f77004e6c358871d9600d
arinochkaaa <153714566+arinochkaaa@users.noreply.github.com>
2024-06-25 15:18:06 +0300

    Add files via upload

---
+++ lab_1/lab_1/lab_1/lab_1.cpp
@@ -0,0 +1,77 @@
+﻿// lab_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <time.h> 
+#include <chrono>
+#include <thread>
+using namespace std;
+
+clock_t time_all = 0;
+clock_t start_r = 0;
+clock_t end_r = 0;
+
+unsigned long long  RecFib(unsigned long long arg_a = 0, unsigned long long  arg_b = 1, unsigned long long  arg_n = 1)
+{
+    start_r = clock();
+    arg_n -= 1;
+    if (arg_n == 0) {
+        return arg_a + arg_b;
+        end_r = clock();
+        time_all += end_r - start_r;
+    }
+    else {
+        unsigned long long  c = arg_a + arg_b;
+        //this_thread::sleep_for(chrono::seconds(1));
+        end_r = clock();
+        time_all += end_r - start_r;
+        return RecFib(arg_b, c, arg_n);
+    }
+}
+
+void time(clock_t time) {
+    //int time_arr[2];
+    double seconds; int minutes = 0;
+    seconds = (double)(time) / CLOCKS_PER_SEC;
+    if (seconds > 60) {
+        for (; seconds > 60;) {
+            minutes++;
+            seconds -= 60;
+        }
+    }
+
+    cout << "Расчетное время циклом " << minutes << " минут " << seconds << " секунд." << "\n";
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    unsigned long long n, a = 0, b = 1, result;
+    cout << "введите порядковый номер числа Фибоначчи:";
+    cin >> n;
+    if (n == 1 || n == 0)
+    {
+        cout << "Через цикл. Результат вашего числа: " << 0 << "\n";
+    }
+    else
+    {
+        clock_t start_c = clock();
+        for (int i = 2; i <= n; i++)
+        {
+            result = a + b;
+            a = b;
+            b = result;
+        }
+        clock_t end_c = clock();
+        time_all = end_c - start_c;
+        time(time_all);
+
+        cout << "Через цикл. Результат вашего числа: " << result << "\n";
+        time_all = 0;
+        result = RecFib(0, 1, n - 1);
+        time(time_all);
+
+        cout << "Через рекурсию. Результат вашего числа: " << result;
+    }
+
+}
---
+++ lab_3/lab_3/lab_3.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include <chrono>
+#include <time.h> 
+
+//void quicksort(int* mas, int first, int last) {     //функция сортировки
+//	int mid, count;
+//	int f = first, l = last;
+//	mid = mas[(f + l) / 2]; 				//вычисление опорного элемента
+//	do
+//	{
+//		while (mas[f] < mid) f++;
+//		while (mas[l] > mid) l--;
+//		if (f <= l) 				//перестановка элементов
+//		{
+//			count = mas[f];
+//			mas[f] = mas[l];
+//			mas[l] = count;
+//			f++;
+//			l--;
+//		}
+//	} while (f < l);
+//	if (first < l) quicksort(mas, first, l);
+//	if (f < last) quicksort(mas, f, last);
+//}
+//
+//
+//void selectionSort(int* a, int n) {
+//	int min;
+//	for (int i = 0; i < n; i++) {
+//		min = i;
+//
+//		for (int j = i + 1; j < n; j++)
+//			min = (a[j] < a[min]) ? j : min;
+//
+//		if (i != min) {
+//			int buf = a[i];
+//			a[i] = a[min];
+//			a[min] = buf;
+//		}
+//	}
+//}
+//
+//void ShakerSort(int* a, int n) {
+//	int left, right, i, temp;
+//	left = 0;
+//	right = n - 1;
+//	while (left < right) {
+//		for (i = right; i >= left; i--) {
+//			if (a[i - 1] > a[i]) {
+//				temp = a[i];
+//				a[i] = a[i - 1];
+//				a[i - 1] = temp;
+//			}
+//		}
+//		left++;
+//		for (i = left; i <= right; i++) {
+//			if (a[i - 1] > a[i]) {
+//				temp = a[i];
+//				a[i] = a[i - 1];
+//				a[i - 1] = temp;
+//			}
+//		}
+//		right--;
+//	}
+//}
+//
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int n, elem, nBuf;
+	std::cout << "введите размер массива\n";
+	std::cin >> n;
+	if (n > 0) {
+		nBuf = n;
+		int* A = new int[n];
+		int* B = new int[n];
+
+		for (int i = 0; i < n; i++)
+		{
+			elem = --nBuf;
+			A[i] = elem;
+			B[i] = elem;
+
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			std::cout << A[i] << ' ';
+		}
+		std::cout << '\n';
+
+		auto timeStartB = std::chrono::high_resolution_clock::now();//bubles
+		int nSwapsB = 1;
+		while (nSwapsB != 0)
+		{
+			nSwapsB = 0;
+			for (int i = 0; i < n - 1; i++)
+			{
+				if (B[i] > B[i + 1])
+				{
+					int temp = B[i];
+					B[i] = B[i + 1];
+					B[i + 1] = temp;
+					nSwapsB++;
+				}
+			}
+		}
+
+	}
+}
